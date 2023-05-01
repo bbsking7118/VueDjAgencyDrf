bfe3391b0afa79c29c0cd545bd4339b33ef09a16
bbsking7118 <bbsking1@naver.com>
2023-05-01 18:24:31 +0900

    first

---
+++ api2/serializer.py
@@ -0,0 +1,16 @@
+# Serializers define the API representation.
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from blog.models import Post
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
