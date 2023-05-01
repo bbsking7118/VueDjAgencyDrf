10fe9962d3ed8013547e2881d75b50c03b067335
bbsking7118 <bbsking1@naver.com>
2023-05-02 00:13:19 +0900

    ...

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,26 @@ from django.db import models
 
 
 class Post(models.Model):
+    title = models.CharField('TITLE', max_length=50)
+    request = models.CharField('Request', max_length=20)
+    price1 = models.CharField('Price1', max_length=20)
+    price2 = models.CharField('Price2', max_length=20, blank=True, null=True)
+    price3 = models.CharField('Price3', max_length=20, blank=True, null=True)
+    kind = models.CharField('Kind', max_length=20)
+    spec = models.CharField('Spec', max_length=50)
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, null=True, help_text='simple one-line text.')
+    agent = models.CharField('Agent', max_length=30, blank=True, null=True)
+    register = models.CharField('Register', max_length=30)
+    image = models.ImageField('IMAGE', upload_to='blog/%Y/%m/', blank=True, null=True)
+    image1 = models.ImageField('IMAGE1', upload_to='blog/%Y/%m/', blank=True, null=True)
+
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
     tags = models.ManyToManyField('Tag', blank=True)
-    title = models.CharField('TITLE', max_length=50)
-    description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple one-line text.')
-    image = models.ImageField('IMAGE', upload_to='blog/%Y/%m/', blank=True, null=True)
     content = models.TextField('CONTENT')
-    create_dt = models.DateTimeField('CREATE DT', auto_now_add=True)
-    update_dt = models.DateTimeField('UPDATE DT', auto_now=True)
     like = models.PositiveSmallIntegerField('LIKE', default=0)
 
+    create_dt = models.DateTimeField('CREATE DT', auto_now_add=True)
+    update_dt = models.DateTimeField('UPDATE DT', auto_now=True)
     class Meta:
         ordering = ('update_dt',)
 
